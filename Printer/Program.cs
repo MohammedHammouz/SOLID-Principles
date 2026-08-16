a6a5c8c6bdfc84b5f535cc2ae6f7830ec3fae9b4
moha327 <m7amad9595@outlook.com>
2026-08-16 13:35:04 +0300

    Add Printer application with  Interface Segregation Principle

---
+++ Printer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printer
+{
+    public interface IPrint
+    {
+        void Print(string content);
+
+    }
+    public interface IScan
+    {
+        void Scan();
+
+    }
+    public interface IFax
+    {
+        void Fax();
+
+    }
+  
+    public class BasicPrinter : IPrint
+    {
+        public void Print(string content)
+        {
+            Console.WriteLine($"The message:{content}");
+        }
+    }
+    public class AdvancePrinter : IPrint,IFax,IScan
+    {
+        public void Print(string content)
+        {
+            Console.WriteLine($"The message:{content}");
+        }
+        public void Scan()
+        {
+            Console.WriteLine("Scanning...");
+        }
+        public void Fax()
+        {
+            Console.WriteLine("Faxing...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicPrinter basicPrinter = new BasicPrinter();
+            basicPrinter.Print("Hi My name's Mohammed");
+            AdvancePrinter advancePrinter = new AdvancePrinter();
+            advancePrinter.Print("Hi My name's Mohammed");
+            advancePrinter.Fax();
+            advancePrinter.Scan();
+            Console.ReadKey();
+        }
+    }
+}
