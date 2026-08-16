8d4f4be1928dcd34208eba39aa3e0860c77ea1c8
moha327 <m7amad9595@outlook.com>
2026-08-16 14:01:52 +0300

    Add Payment Application wit  Interface Segregation Principle

---
+++ Payment/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Payment
+{
+    public interface IPayment
+    {
+        void PayWithCreditCard();
+        void PayWithPayPal();
+        void PayWithBitcoin();
+    }
+    public interface ICreditCardPayment
+    {
+        void PayWithCreditCard();
+    }
+    public interface IPayPalPayment
+    {
+        void PayWithPayPal();
+    }
+    public interface IBitcoinPayment
+    {
+        void PayWithBitcoin();
+    }
+    public class CreditCardPayment : ICreditCardPayment
+    {
+        public void PayWithCreditCard()
+        {
+            Console.WriteLine("Payment with credit card.");
+        }
+    }
+
+    public class PayPalPayment : IPayPalPayment
+    {
+        public void PayWithPayPal()
+        {
+            Console.WriteLine("Payment with PayPal.");
+        }
+
+    }
+    public class BitcoinPayment :IBitcoinPayment
+    {
+        public void PayWithBitcoin()
+        {
+            Console.WriteLine("Payment with Bitcoin.");
+        }
+
+    }
+    public class AllPayments : IBitcoinPayment, IPayPalPayment, ICreditCardPayment
+    {
+        public void PayWithCreditCard()
+        {
+            Console.WriteLine("Payment with credit card.");
+        }
+        public void PayWithPayPal()
+        {
+            Console.WriteLine("Payment with PayPal.");
+        }
+        public void PayWithBitcoin()
+        {
+            Console.WriteLine("Payment with Bitcoin.");
+        }
+
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ICreditCardPayment creditCardPayment = new CreditCardPayment();
+            creditCardPayment.PayWithCreditCard();
+            
+
+            IPayPalPayment payPalPayment = new PayPalPayment();
+            payPalPayment.PayWithPayPal();
+
+            IBitcoinPayment bitcoinPayment = new BitcoinPayment();
+            bitcoinPayment.PayWithBitcoin();
+
+            AllPayments allPayments = new AllPayments();
+            allPayments.PayWithBitcoin();
+            allPayments.PayWithCreditCard();
+            allPayments.PayWithPayPal();
+
+            Console.ReadKey();
+        }
+    }
+}
